3094e6c5ef1754c64d72f13317ead0517b1dc9f0
Jefferson Lam <jlam520@gmail.com>
2014-02-03 13:21:31 -0800

    generated logs model, changed timezone to pacific US

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140128232704) do
+ActiveRecord::Schema.define(version: 20140203205604) do
+
+  create_table "logs", force: true do |t|
+    t.string   "task"
+    t.string   "result"
+    t.datetime "ended_at"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "display_name"
