d0637f92600a000662b60f0d511dd3a9f4546d87
Jefferson Lam <jlam520@gmail.com>
2014-02-05 09:29:24 -0800

    Added 'next' and 'prev' methods to the Log model to help with creating news feed timestamp. Created time stamps for news feed such that the full date only gets printed once for logs that were logged on the same day. Added a 'Today' timestamp also. Removed the Ruby code from the JS times-up functions. Displayed the Task header in the proper places, made sure it doesn't disappear or reappear randomly on page refresh.

--- app/models/log.rb
+++ app/models/log.rb
@@ -4,4 +4,12 @@ class Log < ActiveRecord::Base
   validates :task, :result, length: { maximum:120 }
   validates :task, presence: true, on: :create
   # validates :result, presence: true, on: :update
+
+  def next
+    Log.where("created_at > ?", created_at).where("result IS NOT NULL").order("created_at ASC").first
+  end
+
+  def prev
+    Log.where("created_at < ?", created_at).where("result IS NOT NULL").order("created_at DESC").first
+  end
 end
